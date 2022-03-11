e3e86e1d85ea5d6a68a71e97963219b337dd1760
alancuenca <alan.r.cuenca@gmail.com>
2022-03-10 21:41:56 -0800

    added users table and moved some code to better order in drop tables

--- db/seedData.js
+++ db/seedData.js
@@ -11,8 +11,10 @@ async function dropTables() {
   try {
     console.log("Dropping All Tables...");
     client.query(`
+      DROP TABLE IF EXISTS orders;
+      DROP TABLE IF EXISTS users;      
       DROP TABLE IF EXISTS products;
-      DROP TABLE IF EXISTS users;
+
     `);
 
     console.log("Finished dropping tables!");
@@ -37,11 +39,18 @@ async function buildTables() {
           "imgURL" VARCHAR(255)
         );
 
+        CREATE TABLE users(
+          id SERIAL PRIMARY KEY,
+          username VARCHAR(255) UNIQUE NOT NULL,
+          password VARCHAR(255) NOT NULL,
+          email VARCHAR(255) UNIQUE NOT NULL
+        );
+
         CREATE TABLE orders(
           id SERIAL PRIMARY KEY,
           "creatorId" INTEGER REFERENCES users(id),
           name VARCHAR(255) UNIQUE NOT NULL,
-          subtotal INTEGER,      
+          subtotal INTEGER      
         );
       `);
 
