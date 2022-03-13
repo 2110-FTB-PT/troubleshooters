ac98783d22f38dee478b0afaf77c4a1b54cd885c
alancuenca <alan.r.cuenca@gmail.com>
2022-03-13 11:56:25 -0700

    #9 added login users api

--- api/users.js
+++ api/users.js
@@ -1,5 +1,34 @@
 const express = require('express');
-const usersRouter = express.Router();
+const router = express.Router();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
 const { JWT_SECRET } = process.env;
 const { createUser, getUser, getUserById, getUserByUsername, updateUser } = require('../db');
+
+router.post('/login', async (req, res, next) => {
+    const { username, password } = req.body;
+try{
+    if (!username || !password){
+        next({
+            name: "MissingCredentialsError",
+            message: "Please enter both username and password"
+        });
+    }   
+        const user = await getUserByUsername(username)
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (user && isMatch){
+            const token = jwt.sign(user, JWT_SECRET);
+            res.send({ 
+                token, 
+                message: "Thank you for logging in" 
+            })
+        }else{
+            next({
+                name: "IncorrectCredentialsError",
+                message: "Username or Password incorrect"
+            })
+        }
+    }catch(error){
+        next(error);
+    }
+})
