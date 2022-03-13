820d05a418f9f726fd63dc75ce732ce296368353
DonnyDunamis <mjd_mail@yahoo.com>
2022-03-12 22:52:14 -0800

    Just a little inspiration for us as we build this together

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 const App = () => {
-    return (
-        <div className="App">
-            <h2>Hello World</h2>
-        </div>
-    )
-}
+  return (
+    <div className="App">
+      <h2>
+        "If Music is a Place — then Jazz is the City, Folk is the Wilderness,
+        Rock is the Road, and Classical is a Temple." -Vera Nazarin
+      </h2>
+      <h1>Welcome to Crate Diggers!</h1>
+    </div>
+  );
+};
 
 export default App;
