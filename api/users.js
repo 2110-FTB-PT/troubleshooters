4f3bc174d810debf0743c350ba7dc07a2d40fddd
alancuenca <alan.r.cuenca@gmail.com>
2022-03-13 15:45:37 -0700

    #9 added to api users

--- api/users.js
+++ api/users.js
@@ -3,32 +3,83 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
 const { JWT_SECRET } = process.env;
-const { createUser, getUser, getUserById, getUserByUsername, updateUser } = require('../db');
+const { createUser, getUser, getUserByUsername, updateUser } = require('../db');
+const { route } = require('.');
+
+router.get('/', async (req, res, next) => {
+    const users = await getUser();
+
+    res.send({
+        users
+    })
+});
+
+router.post('/register', async (req, res, next) => {
+    const { username, password, name, email } = req.body
+    try {
+        const _user = await getUserByUsername(username)
+        if (_user) {
+            next({
+                name: 'UserExistsError',
+                message: 'A user by that username already exists'
+            })
+        } else if (password.length < 8) {
+            next({
+                name: 'PasswordLengthError',
+                message: 'Password must be 8 or more characters long'
+            })
+        } else if (!username || !password || !email || !name) {
+            next({
+                name: 'MissingCredentials',
+                message: 'Plese fill all required fields'
+            })
+        } else {
+            const user = await createUser({
+                username,
+                password,
+                name,
+                email
+            });
+            const token = jwt.sign({
+                id: user.id,
+                username
+            }, process.env.JWT_SECRET, {
+                expiresIn: '1w'
+            })
+            res.send({
+                name: 'SuccessRegister',
+                message: 'Thank you for signing up'
+            })
+        }
+    } catch ({ name, message }) {
+        next({ name, message })
+    }
+})
 
 router.post('/login', async (req, res, next) => {
     const { username, password } = req.body;
-try{
-    if (!username || !password){
-        next({
-            name: "MissingCredentialsError",
-            message: "Please enter both username and password"
-        });
-    }   
+    try {
+        if (!username || !password) {
+            next({
+                name: 'MissingCredentialsError',
+                message: 'Please enter both username and password'
+            });
+        }
         const user = await getUserByUsername(username)
         const isMatch = await bcrypt.compare(password, user.password)
-        if (user && isMatch){
+        if (user && isMatch) {
             const token = jwt.sign(user, JWT_SECRET);
-            res.send({ 
-                token, 
-                message: "Thank you for logging in" 
+            res.send({
+                token,
+                message: 'Thank you for logging in'
             })
-        }else{
+        } else {
             next({
-                name: "IncorrectCredentialsError",
-                message: "Username or Password incorrect"
+                name: 'IncorrectCredentialsError',
+                message: 'Username or Password incorrect'
             })
         }
-    }catch(error){
-        next(error);
+    } catch ({ name, message }) {
+        next({ name, message });
     }
 })
