b29d6fdc0cffad15ba4bbb508a33623af06a5e0c
scburkha005 <scburkha005@gmail.com>
2022-03-16 20:17:20 -0700

    db adaptor for adding a category to a single product

--- db/models/product_categories.js
+++ db/models/product_categories.js
@@ -1,7 +1,17 @@
 const client = require('../client')
 
-const addCategoryToProduct = async () => {
+const addCategoryToProduct = async ({ productId, categoryId}) => {
+  try {
+    const { rows: [productCategory] } = await client.query(`
+      INSERT INTO product_categories("productId", "categoryId") 
+      VALUES ($1, $2)
+      RETURNING *;
+    `, [productId, categoryId]);
 
+    return productCategory;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
