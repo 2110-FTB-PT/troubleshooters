0b7af76741cfff2ccfbb61b6637950cc1ac777c4
scburkha005 <scburkha005@gmail.com>
2022-03-17 14:15:50 -0700

    GET /api/products

--- api/products.js
+++ api/products.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const router = express.Router();
+const { getAllProducts } = require('../db/models/products');
 
 router.use((req, res, next) => {
   console.log("A request is being made to /products");
 
   next();
+})
+
+// GET /api/products
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await getAllProducts();
+
+    res.send(products);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 })
 
 module.exports = router;
