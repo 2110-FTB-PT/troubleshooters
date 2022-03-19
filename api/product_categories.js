439a68e6063a7aad5e1e2629fb9edd8dad2d7fa1
scburkha005 <scburkha005@gmail.com>
2022-03-18 19:35:31 -0700

    POST /api/product_categories/:productId/:categoryId

--- api/product_categories.js
+++ api/product_categories.js
@@ -1,10 +1,23 @@
-const express = require('express')
+const express = require('express');
+const { addCategoryToProduct } = require('../db/models/product_categories');
 const router = express.Router();
 
 router.use((req, res, next) => {
   console.log("A request is being made to /product_categories");
 
   next();
+})
+
+// POST /api/product_categories/:productId/:categoryId => adds a category to a product
+router.post('/:productId/:categoryId', async (req, res, next) => {
+  const { productId, categoryId } = req.params;
+  try {
+    const productCategory = await addCategoryToProduct({ productId, categoryId });
+
+    res.send(productCategory)
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 })
 
 module.exports = router;
