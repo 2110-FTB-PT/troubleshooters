e4e06f17c2b7bc1c2c694c5ad77336f6c2362304
scburkha005 <scburkha005@gmail.com>
2022-03-21 14:32:51 -0700

    moved products api calls to separate file

--- src/api/index.js
+++ src/api/index.js
@@ -44,52 +44,6 @@ export const getUser = async (token) => {
   }
 };
 
-export const fetchProducts = async () => {
-  try {
-    const { data } = await axios.get(`${BASE_URL}/products`);
-    return data;
-  } catch (error) {
-    console.error("Error at fetchProducts", error);
-  }
-};
-
-export const addProduct = async (productToAdd, token) => {
-  try {
-    const { data } = await axios.post(`${BASE_URL}/products`, productToAdd, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return data;
-  } catch ({ response }) {
-    console.error("Error at addProduct", response);
-  }
-};
-
-export const updateProduct = async (
-  title,
-  artist,
-  description,
-  price,
-  inventoryQuantity,
-  imgURL,
-  productId
-) => {
-  try {
-    const { data } = await axios.patch(`${BASE_URL}/products/${productId}`, {
-      title,
-      artist,
-      description,
-      price,
-      inventoryQuantity,
-      imgURL,
-    });
-    return data;
-  } catch (error) {
-    console.error("Error at updateProduct", error);
-  }
-};
-
 export const fetchOrders = async () => {
   try {
     const { data } = await axios.get(`${BASE_URL}/orders`);
