cadfa605d6789ef827a4adac96ab2664817c8820
scburkha005 <scburkha005@gmail.com>
2022-03-21 15:37:58 -0700

    renders out a single product either with minimal information OR full information based on whether productId params contains a value or not

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,11 +1,36 @@
-const SingleProduct = ({ product: { title, artist, description, price, imgURL } }) => {
+import { useNavigate, useParams } from "react-router";
+import { useState, useEffect } from "react";
 
+const SingleProduct = ({ product, products }) => {
+  const navigate = useNavigate();
+  const { productId } = useParams();
+  const [singleProduct, setSingleProduct] = useState({});
+
+  useEffect(() => {
+    if (productId) {
+      const [ product ] = products.filter(product => product.id === Number(productId));
+      console.log(product)
+      setSingleProduct(product)
+    }
+  }, [])
+
+  const handleClick = () => {
+    navigate(`/products/${product.id}`)
+  }
+
+  const { title, artist, price, imgURL, description, inventoryQuantity } = product || singleProduct;
   return (
-    <div className='singleProduct'>
+    <div className='singleProduct' onClick={handleClick}>
       <div>{title}</div>
       <div>{artist}</div>
-      <img src={require(`../assets/${imgURL}`)}/>
-      <div>{price}</div>
+      {imgURL && <img src={require(`../assets/${imgURL}`)}/> }
+      {productId && 
+      <>
+        <div>{description}</div>
+        <div>Amount in Stock: {inventoryQuantity}</div>
+      </>
+      }
+      <div>${price}</div>
     </div>
   )
 }
