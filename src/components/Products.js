3af62398aa615783d433f1bcd78fa5c22a3b1de2
scburkha005 <scburkha005@gmail.com>
2022-03-21 14:48:47 -0700

    fetch and setProducts in state

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { getAllProducts } from "../api/productsApi";
+
 const Products = ({ products, setProducts }) => {
+  const fetchProducts = async () => {
+    setProducts(await getAllProducts());
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
 
   return (
     <></>
