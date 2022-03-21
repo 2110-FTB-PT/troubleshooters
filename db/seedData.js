086358e3e6422161c27a77fff5069d5efcc09ac7
scburkha005 <scburkha005@gmail.com>
2022-03-21 15:11:36 -0700

    changed initial seeding of products imgURL to only contain the name of the file

--- db/seedData.js
+++ db/seedData.js
@@ -110,7 +110,7 @@ async function createInitialProducts() {
         description: 'The album is the second installment of a tetralogy about the ongoing saga of the Keywork in The Amory Wars. The Amory Wars is also the name of the graphic novel series written by lead singer Claudio Sanchez that details the events foretold in greater detail. There are three notable singles on this album: "A Favor House Atlantic", "Blood Red Summer" and "In Keeping Secrets of Silent Earth: 3".',
         price: 10.00,
         inventoryQuantity: 13,
-        imgURL: '../assets/In_Keeping_Secrets_of_Silent_Earth_3_cover.jpg'
+        imgURL: 'In_Keeping_Secrets_of_Silent_Earth_3_cover.jpg'
       },
       {
         title: 'always EP',
@@ -118,7 +118,7 @@ async function createInitialProducts() {
         description: '"Always", stylized as "always", is the fourth extended play (EP) by American singer-songwriter keshi. It was released through Island Records on October 23rd, 2020.',
         price: 15.99,
         inventoryQuantity: 20,
-        imgURL: '../assets/Always_EP_Keshi.jpg'
+        imgURL: 'Always_EP_Keshi.jpg'
       },
       {
         title: 'My Way',
@@ -126,7 +126,7 @@ async function createInitialProducts() {
         description: `My Way is an album by American singer Frank Sinatra, released in 1969 on his own Reprise label. The album is mainly a collection of then-contemporary pop songs, such as Simon and Garfunkel's "Mrs. Robinson", and The Beatles' "Yesterday", French songs such as "If You Go Away", and the anthemic title song "My Way", which effectively became Sinatra's theme song in this stage of his career.`,
         price: 14.99,
         inventoryQuantity: 50,
-        imgURL: '../assets/SinatraMyWay.jpg'
+        imgURL: 'SinatraMyWay.jpg'
       },
       {
         title: 'Live at the Apollo',
@@ -135,7 +135,7 @@ async function createInitialProducts() {
         The album is included in Robert Christgau's "Basic Record Library" of 1950s and 1960s recordings, published in Christgau's Record Guide: Rock Albums of the Seventies (1981). In 2000 it was voted number 248 in Colin Larkin's All Time Top 1000 Albums. In 2003, the album was ranked number 25 on Rolling Stone magazine's list of The 500 Greatest Albums of All Time, maintaining the rating in a 2012 revised list, and re-ranking at number 65 in a 2020 reboot of the list. In 2004, it was one of 50 recordings chosen that year by the Library of Congress to be added to the National Recording Registry.`,
         price: 12.99,
         inventoryQuantity: 12,
-        imgURL: '../assets/James_Brown-Live_at_the_Apollo_(album_cover).jpg'
+        imgURL: 'James_Brown-Live_at_the_Apollo_(album_cover).jpg'
       },
       {
         title: 'Let It Bleed',
@@ -143,7 +143,7 @@ async function createInitialProducts() {
         description: `Let It Bleed is the 8th British and 10th American studio album by the English rock band the Rolling Stones, released on 28 November 1969 London Records in the United States and shortly thereafter by Decca Records in the United Kingdom. Released shortly after the band's 1969 American Tour, it is the follow-up to 1968's Beggars Banquet. As with Beggars Banquet, the album marks a return to the group's more blues-sound approach that was prominent in the pre-Aftermath period of their career. Additional sounds on the album draw influence from gospel, country blues and country rock.`,
         price: 12.99,
         inventoryQuantity: 14,
-        imgURL: '../assets/LetitbleedRS.jpg'
+        imgURL: 'LetitbleedRS.jpg'
       },
       {
         title: `All My Friends We're Glorious`,
@@ -151,7 +151,7 @@ async function createInitialProducts() {
         description: `All My Friends We're Glorious (or All My Friends We're Glorious: Death of a Bachelor Tour Live) is the fourth live album by Panic! at the Disco and their first as a solo project, fronted by Brendon Urie. It was released on December 15, 2017, in digital versions and as a limited edition double vinyl LP, and documents the band's 2017 Death of a Bachelor Tour concert tour following the release of their fifth album, Death of a Bachelor.`,
         price: 15.99,
         inventoryQuantity: 6,
-        imgURL: '../assets/Panic_AllMyFriends.jpg'
+        imgURL: 'Panic_AllMyFriends.jpg'
       },
       {
         title: 'Innervisions',
@@ -159,7 +159,7 @@ async function createInitialProducts() {
         description: `Innervisions is the sixteenth studio album by American singer, songwriter and musician Stevie Wonder, released August 3, 1973, on the Tamla label for Motown Records, a landmark recording of his "classic period". It is also regarded as Wonder's transition from Little Stevie Wonder and romantic ballads to a more musically mature, conscious and grown-up artist. With Wonder being the first major artist to experiment with the revolutionary TONTO (The Original New Timbral Orchestra) synth, developed by Malcolm Cecil and Robert Margouleff, and the ARP synthesizer on a large scale, Innervisions became hugely influential on the subsequent future of commercial soul and black music.`,
         price: 10.99,
         inventoryQuantity: 15,
-        imgURL: '../assets/Steviewonder_innervisions.jpg'
+        imgURL: 'Steviewonder_innervisions.jpg'
       },
     ]
     const products = await Promise.all(
