19ef620fc6d62b58550f8fbc46ca51251832ef04
scburkha005 <scburkha005@gmail.com>
2022-03-25 15:50:35 -0700

    modular input field that takes in values and a handleChange and updates state

---
+++ src/shared/Input.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+const Input = ({ name, value, handleChange }) => {
+  // set the initial value of our state to the init value of the state mapped over
+  const [input, setInput] = useState(value)
+  // whenever our input changes, update our state in a higher level component
+  // avoids rerendering from prop changes
+  useEffect(() => {
+    handleChange(input)
+  }, [input])
+
+  // set our type for our input field
+  let typeSetter = '';
+  if (typeof value === 'number') {
+    typeSetter = 'number'
+  } else {
+    typeSetter = 'text'
+  }
+
+  return (
+    <>
+      <input placeholder={name} type={typeSetter} value={input} onChange={e => {
+        if (typeSetter === 'text') {
+          setInput(e.target.value);
+        } else if (typeSetter === 'number') {
+          setInput(Number(e.target.value));
+        }
+      }}/>
+      <label>{name}</label>
+    </>
+  )
+}
+
+export default Input;
