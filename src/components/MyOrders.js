1f093d8b68a5550fdf569c137069306d96289776
DonnyDunamis <mjd_mail@yahoo.com>
2022-03-25 01:04:57 -0700

    created MyOrders component to be available within MyProfile

---
+++ src/components/MyOrders.js
@@ -0,0 +1,58 @@
+import { AddOrder } from "./";
+import { deleteOrder } from "../api";
+import { useNavigate } from "react-router-dom";
+
+const MyOrders = ({ token, user, orders, setOrders }) => {
+  const navigate = useNavigate();
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteOrder(token, id);
+      const newOrdersObject = orders.filter((order) => {
+        return order.id !== id;
+      });
+      setOrders(newOrdersObject);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      {token && <AddOrder token={token} order={order} setOrder={setOrder} />}
+      <h2>My Orders</h2>
+      {orders.map((order) => {
+        return (
+          <div key={order.id}>
+            <div>Customer Number : {orders.creatorId}</div>
+            <div>Subtotal: {orders.subtotal} </div>
+            {user?.id === order.creatorId && (
+              <button onClick={() => navigate(`/orders/${order.id}`)}>
+                {" "}
+                Edit{" "}
+              </button>
+            )}
+            {user?.id === order.creatorId && (
+              <button onClick={() => handleDelete(order.id)}> Delete </button>
+            )}
+            <h2>Items</h2>
+            {order.products.map((product) => {
+              return (
+                <div key={product.id}>
+                  <div>Artist: {product.artist}</div>
+                  <div>Title: {product.title}</div>
+                  <div>Price: {product.price}</div>
+                  <div>Quantity: {product.quantity}</div>
+                  <br></br>
+                </div>
+              );
+            })}
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default MyOrders;
