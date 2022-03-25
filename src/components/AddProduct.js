0749e6ec44cca0b337591e68f228e192c66723dd
scburkha005 <scburkha005@gmail.com>
2022-03-25 15:54:28 -0700

    add product component

---
+++ src/components/AddProduct.js
@@ -0,0 +1,13 @@
+import ProductForm from "./ProductForm";
+
+const AddProduct = () => {
+
+  return (
+    <>
+      <h2>Add Product</h2>
+      <ProductForm />
+    </>
+  )
+}
+
+export default AddProduct;
