000a3c1ec40b210a20019ce759cedd94b7f42016
alancuenca <alan.r.cuenca@gmail.com>
2022-03-30 19:58:28 -0700

    rebase continue

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,19 +1,12 @@
 const HomePage = () => {
   return (
-    <ul className="home">
-      <br></br>
-      <h1>Welcome to Crate Diggers!</h1>
-      <div className="container2">
-        <img
-        src={splash}
-        alt="digging"
-        id="splash"
-        />
-        <div className="overlay">
-    <h2 className="text2">"Where words fail, music speaks" -Hans Christian Andersen</h2>
-    </div>
+    <div className="home">
+      <header>
+      <div className="welcome">
+        Welcome Crate Diggers!
       </div>
-    </ul>
+    </header>
+    </div>
   );
 };
 export default HomePage;
