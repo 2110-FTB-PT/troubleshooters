3942470e91ea679e913e132fb38c64e7b1fd707b
DonnyDunamis <mjd_mail@yahoo.com>
2022-03-31 21:37:01 -0700

    created stripeCheckout function to axios fetch checkout from the server

--- src/api/index.js
+++ src/api/index.js
@@ -157,3 +157,15 @@ export const deleteOrderProduct = async (token, orderProductId) => {
     console.error("Error at deleteOrderProduct", error);
   }
 };
+
+export const stripeCheckout = async (items) => {
+  try {
+    const { data } = await axios.post(`/api/orders/checkout`, {
+      items,
+    });
+    window.location = data.url;
+    return data;
+  } catch (error) {
+    console.error("Error at stripeCheckout", error);
+  }
+};
