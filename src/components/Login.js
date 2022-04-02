698516924ba46154261e561c4c7cdb228daca64f
alancuenca <alan.r.cuenca@gmail.com>
2022-04-02 11:37:33 -0700

    login and reg styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,34 +4,34 @@ import { useNavigate, Link } from 'react-router-dom';
 import { useUserContext } from "../context/UserContext";
 import Button from "../shared/Button";
 
-const Login =  () => { 
+const Login = () => {
     const { setToken } = useUserContext()
 
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
-    const handleLogin = async (e)=> {
-       e.preventDefault();
-       try {
-        const [token, message] = await login(username,password);
-        setMessage[message];
-        setToken(token); 
-        navigate('/')
-       } catch (error) {
-           console.error(error)
-           setMessage(error.response.data.message)
-       }
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const [token, message] = await login(username, password);
+            setMessage[message];
+            setToken(token);
+            navigate('/')
+        } catch (error) {
+            console.error(error)
+            setMessage(error.response.data.message)
+        }
     }
     return (
-        <form>
-            <h1>Login</h1>
+        <form className="loginForm">
+            <h1 className="Login">Login</h1>
             {message && <div>{message}</div>}
-            <input placeholder="username" value={username} onChange={(event) => {setUsername(event.target.value)}}/>
-            <input placeholder="password" type="password" value={password} onChange={(event) => {setPassword(event.target.value)}}/>
+            <input className="loginBtn" placeholder="username" value={username} onChange={(event) => { setUsername(event.target.value) }} />
+            <input className="loginBtn" placeholder="password" type="password" value={password} onChange={(event) => { setPassword(event.target.value) }} />
             <Button onClick={handleLogin}>Login</Button>
             <div><Link to={'/register'}>Don't have an account? Sign up.</Link></div>
-        </form> 
+        </form>
     );
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { register } from '../api';
 import { useNavigate, Link } from 'react-router-dom';
 import { useUserContext } from '../context/UserContext';
@@ -8,34 +8,33 @@ const Register = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const  [error, setError] = useState('');
+    const [error, setError] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const { setToken } = useUserContext();
-    const submit = async (e)=> {
+    const submit = async (e) => {
         e.preventDefault();
         try {
-            const [newToken,message] = await register(username,password,email);
+            const [newToken, message] = await register(username, password, email);
             setToken(newToken);
             setMessage(message);
             navigate('/');
         }
-        catch(error) {
+        catch (error) {
             console.error(error);
             console.log(error.response);
             setMessage(error.response.data.message);
         }
-    } 
+    }
     return (
-        <form>
-            <h1>Register</h1>
+        <form className="loginForm">
+            <h1 className="Login">Register</h1>
             {message && <div>{message}</div>}
-            <input placeholder="email" value={email} onChange={(event) => {setEmail(event.target.value)}}/>
-            <input placeholder="username" value={username} onChange={(event) => {setUsername(event.target.value)}}/>
-            <input placeholder="password" type="password" value={password} onChange={(event) => {setPassword(event.target.value)}}/>
+            <input className="loginBtn" placeholder="email" value={email} onChange={(event) => { setEmail(event.target.value) }} />
+            <input className="loginBtn" placeholder="username" value={username} onChange={(event) => { setUsername(event.target.value) }} />
+            <input className="loginBtn" placeholder="password" type="password" value={password} onChange={(event) => { setPassword(event.target.value) }} />
             <Button onClick={submit} disabled={username.length === 0 || password.length === 0}>Register</Button>
             <div><Link to={'/login'}>Already have an account? Log in.</Link></div>
-
         </form>
     );
 }
