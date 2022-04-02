ceb2895df1fc4ebd2d4a2c58046369a74299c95e
scburkha005 <scburkha005@gmail.com>
2022-04-01 19:38:05 -0700

    addCategoryToProduct api call

--- src/api/categoryApi.js
+++ src/api/categoryApi.js
@@ -9,3 +9,17 @@ export const getAllCategories = async () => {
     console.error(error);
   }
 }
+
+export const addCategoryToProduct = async (productId, categoryId, token) => {
+  try {
+    const { data: category } = await axios.post(`/api/product_categories/${productId}/${categoryId}`, {}, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    return category;
+  } catch (error) {
+    console.error(error);
+  }
+}
