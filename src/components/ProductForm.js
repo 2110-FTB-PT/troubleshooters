7913086b4c3999f9c2ad8c953e5446e427ed140b
scburkha005 <scburkha005@gmail.com>
2022-04-01 17:50:12 -0700

    select field properly allows multi selection, and updates categoryIds state

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,13 +1,48 @@
 import InputFields from "../shared/InputFields";
 import Button from "../shared/Button";
+import { useState } from 'react';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
 import "./ProductForm.css";
 
-const ProductForm = ({ state, setState, handleSubmit }) => {
+const ProductForm = ({ state, setState, handleSubmit, categories, storeCategories }) => {
+  const [categoryIds, setCategoryIds] = useState([]);
+
+  const handleChange = (event) => {
+    setCategoryIds(event.target.value)
+  }
 
   return (
-    <form onSubmit={handleSubmit} className="product-form">
+    <form className="product-form">
       <InputFields state={state} setState={setState} />
-      <Button>Confirm Product</Button>
+      <InputLabel id="demo-multiple-checkbox-label">Categories</InputLabel>
+      <Select
+        labelId="demo-multiple-checkbox-label"
+        multiple
+        value={categoryIds}
+        onChange={handleChange}
+        input={<OutlinedInput label="Categories" />}
+        renderValue={(selectedCategoryIds) => {
+          const filteredCategories = categories.filter(category => selectedCategoryIds.includes(category.id));
+          let categoryNames = [];
+          filteredCategories.forEach(category => {
+            categoryNames.push(category.name);
+          })
+          return categoryNames.join(', ');
+        }}
+      >
+        {categories.map((category) => (
+          <MenuItem key={category.name} value={category.id}>
+            <Checkbox checked={categoryIds.includes(category.id)} />
+            <ListItemText primary={category.name} />
+          </MenuItem>
+        ))}
+      </Select>
+      <Button onClick={handleSubmit}>Confirm Product</Button>
     </form>
   )
 }
