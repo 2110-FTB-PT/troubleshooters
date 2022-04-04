6f205c7f2f88f3aadbfe2bd3e7e9ff21de199b17
scburkha005 <scburkha005@gmail.com>
2022-04-03 23:08:34 -0700

    fetches and displays reviews by user

---
+++ src/components/MyReviews.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { fetchReviewByUser } from "../api/ReviewApi";
+import { useUserContext } from "../context/UserContext";
+import Card from "../shared/Card";
+import SingleReview from "./SingleReview";
+
+const MyReviews = () => {
+  const { user } = useUserContext();
+  const [reviews, setReviews] = useState([]);
+
+  const handleFetchUserReviews = async () => {
+    try {
+      if (user) {
+        const userReviews = await fetchReviewByUser(user);
+        setReviews(userReviews);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    handleFetchUserReviews();
+  }, [user]);
+
+  return (
+    <>
+    <h2>My Reviews</h2>
+      {reviews?.map(review => {
+        return (
+          <Card>
+            <SingleReview review={review}/>
+          </Card>
+        )
+      })}
+    </>
+  )
+}
+
+export default MyReviews;
